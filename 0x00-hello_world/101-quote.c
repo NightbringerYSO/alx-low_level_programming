a8cd6147cd12f4c31090f27d2642d6966894beba
belal kassem <belalmamdouh61@gmail.com>
2023-03-17 02:37:31 +0200

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C programm that prints with printf
+ *
+ * Return: always 1 (Successful)
+*/
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}

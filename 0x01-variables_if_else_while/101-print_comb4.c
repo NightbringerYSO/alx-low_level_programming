8017113b99dedd08b1a273fba981599e72aeddb3
belal kassem <belalmamdouh61@gmail.com>
2023-03-21 01:44:48 +0200

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print 012, 013, ...,  789
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 0, n1, n2;
+
+	while (n <= 9)
+	{
+		n1 = 0;
+		while (n1 <= 9)
+		{
+			n2 = 0;
+			while (n2 <= 9)
+			{
+				if (n != n1 && n < n1 && n1 != n2 && n1 < n2)
+				{
+					putchar(n + 48);
+					putchar(n1 + 48);
+					putchar(n2 + 48);
+
+					if (n + n1 + n2 <= 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				n2++
+			}
+			n1++;
+		}
+		n++
+	}
+	putchar('\n');
+
+	return (0);
+}

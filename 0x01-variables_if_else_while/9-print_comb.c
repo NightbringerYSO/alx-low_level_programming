f4a63b6ce21294de3a14bafd7f5c841dbd997359
belal kassem <belalmamdouh61@gmail.com>
2023-03-21 00:57:47 +0200

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A function to print signel digit ascendingly followed by a ,
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		puthcar(n, ",");
+	}
+	putchar('\n');
+
+	return (0);
+}

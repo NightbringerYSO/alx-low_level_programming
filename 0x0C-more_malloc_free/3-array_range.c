030b0804abcd30482a436151e135e9fdeb9d9314
belal kassem <belalmamdouh61@gmail.com>
2023-04-13 16:12:59 -0700

    space required

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	len = max - min + 1;
 	ptr = malloc(sizeof(int) * len);
-	if(!ptr)
+	if (!ptr)
 		return (NULL);
 	for (i = 0; i < len; i++)
 		ptr[i] = min++;

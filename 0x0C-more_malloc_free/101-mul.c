355dbd54293eafc8dc73eb566ccad94aba248eb7
belal kassem <belalmamdouh61@gmail.com>
2023-04-13 16:51:27 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: pointer to the sting to print
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: cahr type string
+ * Return: integer converted
+ */
+
+int _atoi(const char *s)
+{
+	int sign = 1;
+	unsigned long int resp = 0, firstNum, i;
+
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+		return (sign * resp);
+}
+
+/**
+ * print_int - prints an int
+ * @n: int
+ * Retrun: 0
+ */
+
+void print_int(unsigned long int n)
+{
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+	;
+
+	for (; disvisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + res[);
+	}
+}
+
+/**
+ * main - print the result of multiplication, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		_puts("Error ");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+
+	return (0);
+}
